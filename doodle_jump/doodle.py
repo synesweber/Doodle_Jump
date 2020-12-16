82e830ebbfed5a0a696ac0ad67bf71aab5f053ef
synes <synes.weber@stud.nksa.ch>
2020-12-16 10:22:12 +0100

    v1

---
+++ doodle_jump/doodle.py
@@ -0,0 +1,11 @@
+import random
+import paygame
+
+class Doodle:
+    def __init__(self):
+        self.jump = jump
+        self.pos = pos
+        self.img = pygame.transform.scale(pygame.load(""),())
+
+
+
---
+++ doodle_jump/steps.py
@@ -0,0 +1,10 @@
+import random
+import pygame
+
+class Steps:
+    def __init__(self):
+        self.pos = pos
+        self.color = color
+        self.speed = speed
+
+
