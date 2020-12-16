d95452bb1d9629673e52ffa0c5152df54d54a7ad
marco <marco.widmer@stud.nksa.ch>
2020-12-16 13:57:26 +0100

    wrgf

--- doodle_jump/doodle.py
+++ doodle_jump/doodle.py
@@ -1,9 +1,10 @@
 import pygame
 
+
 class Doodle:
     def __init__(self):
         self.img = pygame.transform.scale(
-            pygame.image.load("./images/penguin.png"), (40, 40))
-       pass
+           pygame.image.load("./images/penguin.png"), (40, 40))
+
 
 
--- doodle_jump/main.py
+++ doodle_jump/main.py
@@ -1,5 +1,7 @@
 import pygame
 
+import random
+
 from doodle_jump.doodle import Doodle
 
 pygame.init()
@@ -8,6 +10,8 @@ width = 600
 height = 800
 background_color = (255, 255, 255)
 color = (255, 0, 0)
+# x = random.randint(0, 600)
+# y = random.randint(0, 800)
 
 doodle1 = Doodle()
 
@@ -17,20 +21,17 @@ pygame.display.flip()
 
 pygame.display.set_caption("Doodle Jump")
 
-
-
-
 run = True
 
 while run:
     for event in pygame.event.get():
-        pygame.draw.rect(win, color, [200, 700, 75, 10], False)
+        # pygame.draw.rect(win, color, [randint(0, 600), randint(0, 700), 75, 10], False)
+        x = random.randint(0, 600)
+        y = random.randint(0, 800)
+        pygame.draw.rect(win, color, [x, y, 75, 10], False)
         pygame.display.flip()
         if event.type == pygame.QUIT:
             run = False
-
-
-
 
 pygame.quit()
 
