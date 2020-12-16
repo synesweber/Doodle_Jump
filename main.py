ca728e3d1bc57727bf0e8119e85f8987a41c6e09
synes <synes.weber@stud.nksa.ch>
2020-12-16 10:30:45 +0100

    v1

---
+++ main.py
@@ -0,0 +1,21 @@
+    import pygame as pygame
+
+from doodle_jump.doodle import Doodle
+
+
+pygame.init()
+
+width = 200
+height = 500
+
+
+win = pygame.display.set_mode(width, height)
+pygame.display.set_caption("Doodle Jump")
+
+run = True
+
+#doodle1 = Doodle(50,100)
+
+
+
+
