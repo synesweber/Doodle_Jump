5d59f69ed458acad09ee9f36cac920f9ba0fe6b8
synes <synes.weber@stud.nksa.ch>
2020-12-16 10:44:54 +0100

    Version 1

--- doodle_jump/main.py
+++ doodle_jump/main.py
@@ -1,15 +1,15 @@
-import pygame as pygame
+import pygame
 
-
-
+from doodle_jump.doodle import Doodle
 
 pygame.init()
 
 width = 200
 height = 500
 
+doodle1 = Doodle()
 
-win = pygame.display.set_mode(width, height)
+win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Doodle Jump")
 
 run = True
