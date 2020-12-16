f2e9ab8f0254b46987a872a02cb2976b89c235d1
synes <synes.weber@stud.nksa.ch>
2020-12-16 11:04:35 +0100

    Version 2

--- doodle_jump/doodle.py
+++ doodle_jump/doodle.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Doodle:
-    def __init__(self):
+    def __init__(self, jump, pos):
         self.jump = jump
         self.pos = pos
         self.img = pygame.transform.scale(pygame.load(""),())
