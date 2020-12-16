8ff503de68a64b0fabc90081cb7cabaded1ddbce
synes <synes.weber@stud.nksa.ch>
2020-12-16 13:22:31 +0100

    11

--- doodle_jump/doodle.py
+++ doodle_jump/doodle.py
@@ -2,8 +2,8 @@ import pygame
 
 class Doodle:
     def __init__(self):
-        self.img = pygame.transform.scale(
-            pygame.image.load("./images/penguin.png"), (40, 40))
+        self.img = pygame.transform.scale(pygame.image.load("./images/penguin.png"), (40, 40))
+
        pass
 
 
