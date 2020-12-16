0c84800193f0470fc2422f4da51c96d24f77087d
marco <marco.widmer@stud.nksa.ch>
2020-12-16 13:12:48 +0100

    wrgf

--- doodle_jump/main.py
+++ doodle_jump/main.py
@@ -1,13 +1,13 @@
 import pygame
 
-
 from doodle_jump.doodle import Doodle
 
 pygame.init()
 
 width = 600
 height = 800
-background_color = (134, 134, 154)
+background_color = (255, 255, 255)
+
 doodle1 = Doodle()
 
 win = pygame.display.set_mode((width, height))
@@ -16,13 +16,17 @@ pygame.display.flip()
 
 pygame.display.set_caption("Doodle Jump")
 
+pygame.draw.rect(win, [red, green, blue], [left, top, width, 100], filled)
+# win.fill([255, 255, 255])
+# pygame.display.flip()
+
 run = True
 
 while run:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
 
 
 pygame.quit()
--- doodle_jump/steps.py
+++ doodle_jump/steps.py
@@ -1,4 +1,8 @@
+from random import random
+
 import pygame
+import self as self
+
 
 class Steps:
     def __init__(self, pos, color, speed):
@@ -6,4 +10,11 @@ class Steps:
         self.color = color
         self.speed = speed
 
+        self.color = (255, 0, 0)
 
+    # def __pos__(self):
+    #     pass
+
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (100, 100, 50, 50))
